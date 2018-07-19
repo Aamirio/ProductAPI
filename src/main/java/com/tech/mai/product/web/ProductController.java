77b0c1b44d06f03254f6681c91dd0339ba3826a4
c0251411 <aamir.idrees@santander.co.uk>
2018-07-19 14:31:02 +0100

    Added exception handling in controller rather than using advice

--- src/main/java/com/tech/mai/product/web/ProductController.java
+++ src/main/java/com/tech/mai/product/web/ProductController.java
@@ -3,11 +3,10 @@ package com.tech.mai.product.web;
 import com.tech.mai.product.domain.Product;
 import com.tech.mai.product.domain.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Provisions service endpoints and handles all requests
@@ -17,13 +16,21 @@ public class ProductController {
 
     @Autowired
     ProductRepository repository;
+    private String errorMsg;
 
     @GetMapping(path = "/product/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Product getProduct(@PathVariable String id) {
         Product product = repository.findById(id);
         if (product == null)  {
-            throw new ProductNotFoundException(String.format("Product not found for %s", id));
+            errorMsg = String.format("Product not found for %s", id);
+            throw new ProductNotFoundException(errorMsg);
         }
         return product;
+    }
+
+    @ExceptionHandler( {ProductNotFoundException.class} )
+    @ResponseBody
+    public ResponseEntity<Error> handleException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Error(errorMsg, 400001));
     }
 }
--- src/main/java/com/tech/mai/product/web/ProductNotFoundException.java
+++ src/main/java/com/tech/mai/product/web/ProductNotFoundException.java
@@ -1,8 +1,12 @@
 package com.tech.mai.product.web;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by c0251411 on 18/07/2018.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
 
     public ProductNotFoundException(String message) {
