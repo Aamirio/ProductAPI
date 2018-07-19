bce1e3ccfcf08b3545bd443029f5a682bfc75765
c0251411 <aamir.idrees@santander.co.uk>
2018-07-19 14:37:36 +0100

    Refining controller advice

--- src/test/java/com/tech/mai/product/web/ExceptionControllerAdvice.java
+++ src/test/java/com/tech/mai/product/web/ExceptionControllerAdvice.java
@@ -1,19 +1,17 @@
 package com.tech.mai.product.web;
 
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {ProductNotFoundException.class})
-    protected ResponseEntity<Object> handleProductNotFoundException(RuntimeException ex, WebRequest request) {
+    protected ResponseEntity<Error> handleProductNotFoundException(RuntimeException ex) {
 
         // Cast the exception to Domain Exception
         // Note - spring guarantees the exception passed here matches the annotation
@@ -21,8 +19,9 @@ public class ExceptionControllerAdvice extends ResponseEntityExceptionHandler {
 
         // Change the response to use the correct status code
         // The response body is a JSON encoded Error
-        return handleExceptionInternal(pEx, new Error(pEx.getMessage(), pEx.getCode()),
-                new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new Error(pEx.getMessage(), pEx.getCode()));
     }
 
 }
